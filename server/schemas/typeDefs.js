0404070c8e3069c7567ca09d7528af56d41df497
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-09-08 22:43:18 -0700

    defined queries and mutations in typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,46 @@
 const typeDefs = `
+    
+    type User {
+        _id: String!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: String!
+        user: User!
+    }
+
+    type Query {
+        me: User!
+    }
+
+    input SaveBookInput {
+        authors: [String]
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: SaveBookInput!): User
+        removeBook(bookId: String!): User
+    }
 `;
 
 module.exports = typeDefs;
