4fb5fcab60c215a1a79a64d45bf235a59c48217d
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-09-08 22:50:27 -0700

    installed Apollo server and fixed server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 // const routes = require('./routes');
